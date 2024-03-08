2828e7e524c3497138306d1a23c277120ad574d7
quezia <quezia.emanuelly2003@gmail.com>
2024-03-08 18:32:48 -0300

    apagando algumas funções

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -28,48 +28,4 @@ class QueryBuilder
             die($e->getMessage());
         }
     }
-
-    public function insert($table, $parameters)
-    {
-        // INSERT INTO table_name (column1, column2, column3,...columnN) VALUES (value1, value2, value3,...valueN)
-        $sql = sprintf('INSERT INTO %s (%s) VALUES (%s)', 
-            $table,
-            implode(', ', array_keys($parameters)),
-            ':' . implode(', :', array_keys($parameters))
-        );
-
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute($parameters);
-
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
-
-    public function update($table, $id, $parameters)
-    {
-        // UPDATE tabela SET coluna_1 = novo_valor_1, coluna_2 = novo_valor_2 WHERE condicao-de-
-        $sql = sprintf('UPDATE %s SET %s WHERE id = %s', 
-            $table,
-            implode(', ', array_map(function($param) {
-                return $param . ' = :' . $param;
-            }, array_keys($parameters))),
-            $id
-        );
-
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute($parameters);
-
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
-
-    public function delete($table, $id)
-    {
-        // DELETE FROM tabela WHERE condicao_de_busca
-        
-    }
 }
